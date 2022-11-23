303ebe8d38c3924c030667165c015f0c188a34b9
PASCAIL Guillaume <pascailguillaume@gmail.com>
2022-11-23 14:40:52 +0100

    TP12Liste

---
+++ src/main/java/tp12list/Program.java
@@ -0,0 +1,67 @@
+package tp12list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        ArrayList<String> liste = new ArrayList<String>();
+
+        //Ajout à la liste
+        liste.add("Java");
+        liste.add("Javascript");
+        liste.add("C#");
+        liste.add("PHP");
+        liste.add("Python");
+        liste.add("Basic");
+        liste.add("Cobol");
+        liste.add("C");
+        liste.add("C++");
+        liste.add("Pascal");
+        liste.add("Fortran");
+
+        System.out.println(liste.size());
+        System.out.println(liste.get(10));
+        System.out.println(liste.get(0));
+        liste.set(0, "Ada");
+        System.out.println(liste.get(0));
+
+        System.out.println(liste.toString());
+
+
+        /*for (int i = 0; i < liste.size(); i++) {
+            System.out.println(liste.get(i));
+        }
+
+        for (String unLanguage:liste) {
+            System.out.println(unLanguage);
+        }
+
+        Collections.sort(liste);
+        liste.toString();
+
+        liste.remove("Cobol");*/
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Saisissez un language :");
+        String lang = sc.next();
+        if(liste.contains(lang)){
+            System.out.println("Le language " + lang + " est présent dans la liste !");
+        }else{
+            System.out.println("Le language " + lang + " est absent de la liste !");
+        }
+
+        System.out.println("Méthode indexOf");
+
+        System.out.println("Saisissez un language :");
+        String lang2 = sc.next();
+
+        if(liste.indexOf(lang2) != -1){
+            System.out.println("Le language " + lang2 + " est présent dans la liste !");
+        }else{
+            System.out.println("Le language " + lang2 + " est absent de la liste !");
+        }
+
+    }
+}
